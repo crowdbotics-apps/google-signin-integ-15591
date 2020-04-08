70aa18ceba10766ef194b1910196f0eb6da158d8
Crowdbotics <team@crowdbotics.com>
2020-04-08 11:58:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/google_signin_integ_15591/MainActivity.java
@@ -0,0 +1,15 @@
+package com.google_signin_integ_15591;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "google_signin_integ_15591";
+    }
+}
